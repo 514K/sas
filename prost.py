11360e7ea000d1fb53a550f18625e15f6f3b6e63
vaskodin <46847491+vaskodin@users.noreply.github.com>
2019-01-27 02:14:00 +0300

    Update prost.py

--- prost.py
+++ prost.py
@@ -2,4 +2,4 @@ import requests
 url = 'http://oreluniver.ru/schedule/'
 requests.get(url)
 req = requests.get(url).text
-print(req[:600]) #отображение 100 символов
+print(req[:600]) #отображение 600 символов
